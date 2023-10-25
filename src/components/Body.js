bba63c2bb51e1c48cc770c131251be158245f6fb
sudhirzuge <sudhirzuge2000@gmail.com>
2023-10-25 22:44:46 +0530

    props changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,12 +3,21 @@ import RestaurantCard from "./RestaurantCard";
 
 const Body=()=>{
     return(
+    <>
+    <div  className="searchcontainer">
+      <input type="text" 
+      className="searchinput"
+       placeholder="search here"
+        value=""
+       />
+       <button className="search-btn">Search</button>
+    </div>
     <div className="body">
      {restaurantList.map((restaurant)=>{
       return <RestaurantCard {...restaurant.data} key={restaurant.data.id}/>;
-     })
-      }
+     })}
     </div>
+    </>
     );
   };
 
