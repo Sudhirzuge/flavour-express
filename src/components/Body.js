b4b41127df69af9bb8ef124d5feb7798fb3f01db
sudhirzuge <sudhirzuge2000@gmail.com>
2023-10-27 07:52:44 +0530

    Hooks & working of search button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,92 @@
 import {restaurantList} from "../constants";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
+import { useState,useEffect } from "react";
+
+function filterData(searchtext,restaurants) {
+
+  const filter=restaurants.filter((restaurant)=>
+  restaurant?.data?.name?.toLowerCase()?.includes(searchtext.toLowerCase())
+
+  );
+  return filter;
+
+}
+
+
+//never use index as key use if you don't have any unique key in database
 
 const Body=()=>{
-    return(
+
+  const [searchtext,setsearchtext]=useState([]);
+  const [filteredrestaurants,setfilteredrestaurants]=useState([]);
+  const[allrestaurants,setallrestaurants]=useState();
+  
+
+  //empty dependencies-once after renders because it contains callback function
+  //dependency array[searchtext]-once after renders and +every time renders(my search text changes)
+
+  useEffect(()=>{
+    //Api call
+    getRestaurants();
+
+  },[]);
+
+  async function getRestaurants(){
+    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3164945&lng=78.03219179999999&page_type=DESKTOP_WEB_LISTING")
+    const json=await data.json();
+    console.log(json);
+    setallrestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setfilteredrestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+  console.log("render()");
+
+  //Conditional Rendering
+  //if restaurant is empty=>shimmer UI
+  //if restaurant has data=>actual data UI
+
+ //Early return(when we don't want to render)
+
+ if(!allrestaurants)
+ return null;
+
+ if(filteredrestaurants?.length===0)
+ return <h1>No match found for your search!!</h1>
+
+    return allrestaurants.length===0 ? (<Shimmer/>
+    ):(
     <>
-    <div  className="searchcontainer">
-      <input type="text" 
-      className="searchinput"
-       placeholder="search here"
-        value=""
+     <div className="Search-container">
+     <input 
+      type="text"
+      className="search-input"
+      placeholder="search"
+      value={searchtext}
+      onChange={(e)=>{setsearchtext(e.target.value)}}
        />
-       <button className="search-btn">Search</button>
+       <button className="Search-btn" onClick={()=>{
+        //need to filter the Data
+      const data= filterData(searchtext,allrestaurants);
+        //update the state-restaurants
+
+        setfilteredrestaurants(data);
+      
+       }}>Search</button>
     </div>
     <div className="body">
-     {restaurantList.map((restaurant)=>{
+     {
+
+     filteredrestaurants.map((restaurant)=>
+     {
+
       return <RestaurantCard {...restaurant.data} key={restaurant.data.id}/>;
-     })}
+     
+    }
+     )
+      
+     }
     </div>
     </>
     );
   };
-
   export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,14 @@
-export const Title = () => {
+import { useState } from "react";
+
+//import { useState } from "react";
+
+const LogedInUser=()=>{
+  //API call to check LogedIn
+  return true;
+
+}
+
+ export const Title = () => {
     return (
     <a href="/">
     <img src="https://yt3.googleusercontent.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj" alt="logo"
@@ -8,9 +18,16 @@ export const Title = () => {
   };
 
   const Header = function (){
+
+    const[IsLogedIn,setIsLogedIn]=useState(true);
+  //How to use useState in react?
+  //const [title,settitle]=useState("Food Villa");
+  //<h1>{title}</h1>;
+  //<button onClick={()=>settitle("New Food App")}>Title changes</button>
     return (
       <div className = "Header">
         <Title/>
+      
         <div className="nav-items">
           <ul>
             <li>Home</li>
@@ -19,8 +36,11 @@ export const Title = () => {
             <li>Cart</li>
           </ul>
         </div>
+        {IsLogedIn?(<button onClick={()=>setIsLogedIn(false)}>LogOut</button>)
+        :(<button onClick={()=> setIsLogedIn(true)}>Login</button>)}
       </div>
     );
   };
 
   export default Header;
+
